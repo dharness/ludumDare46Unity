8af881a0319ccdd8e22b91705eaca43c1c88fffa
dharness <dharness.engineer@gmail.com>
2020-04-19 19:29:08 -0400

    .

--- Assets/CloudMovementController.cs
+++ Assets/CloudMovementController.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class CloudMovementController : MonoBehaviour
 {
     public Camera camera;
-    public Rigidbody rigidbody;
+    public Rigidbody rb;
+    Vector3 movement, forward, right;
+    public float moveSpeed = 4;
 
     void Start()
     {
-        
+        forward = camera.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0f, 90f, 0f)) * forward;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        if(Input.anyKey)
+        {
+            var horz = Input.GetAxis("Horizontal");
+            var vert = Input.GetAxis("Vertical");
+            var rightMovement = right * horz;
+            var upMovement = forward * vert;
+            var heading = Vector3.Normalize(rightMovement + upMovement);
 
-    void FixedUpdate()
-    {
+            var goingForwardAmount = Vector3.Magnitude(forward - heading); 
+            Debug.Log("forward");
+            Debug.Log(forward);
+            Debug.Log("goingForwardAmount");
+            Debug.Log(goingForwardAmount);
+            //transform.position = transform.position + (heading * moveSpeed * Time.deltaTime);
+            transform.position = transform.position + (heading * (1 + Mathf.Abs(vert)) * moveSpeed * Time.deltaTime);
+            Debug.DrawLine(Vector3.zero, forward, Color.red, 2.5f);
+            Debug.DrawLine(Vector3.zero, heading, Color.blue, 2.5f);
+        }
     }
 }
