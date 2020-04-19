027a3a12df3f1961937769a2fb5cb9aea54a54d8
dharness <dharness.engineer@gmail.com>
2020-04-19 16:41:14 -0400

    .

---
+++ Assets/CloudMovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovementController : MonoBehaviour
+{
+    public Camera camera;
+    public Rigidbody rigidbody;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+    }
+}
