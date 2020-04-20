641cf20aa134accc946468ac0b20e0328f555a59
dharness <dharness.engineer@gmail.com>
2020-04-20 15:57:11 -0400

    .

---
+++ Assets/Scripts/GrabbableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+        rb.isKinematic = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
